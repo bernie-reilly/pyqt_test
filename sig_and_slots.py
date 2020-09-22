f06c288d51a42d781183140c670225f364dbe937
Bernie Reilly <bernard.reilly2@prattwhitney.com>
2020-09-22 13:18:49 -0400

    updates

--- sig_and_slots.py
+++ sig_and_slots.py
@@ -1,13 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets
 
-class MainWindow(QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
-    # SIGNAL: The connected function will be called whenever the window
+        
+        # SIGNAL: The connected function will be called whenever the window
         # title is changed. The new title will be passed to the function.
         self.windowTitleChanged.connect(self.onWindowTitleChange)
 
@@ -30,21 +31,21 @@ class MainWindow(QMainWindow):
         # This sets the window title which will trigger all the above signals
         # sending the new title to the attached functions or lambdas as the
         # first parameter.
-
-
-        self.setWindowTitle("My SUPER app")
-
-        label = QLabel("this is a pyqt5 label")
+        self.setWindowTitle("My Awesome App")
+        
+        label = QLabel("THIS IS AWESOME!!!")
         label.setAlignment(Qt.AlignCenter)
 
         self.setCentralWidget(label)
+        
+        
+    # SLOT: This accepts a string, e.g. the window title, and prints it
+    def onWindowTitleChange(self, s):
+        print(s)
 
-def onWindowTitleChange(self, s):
-    print(s)
-
-# SLOT: This has default parameters and can be called without a value
-def my_custom_fn(self, a="HELLLO!", b=5):
-    print(a, b)
+    # SLOT: This has default parameters and can be called without a value
+    def my_custom_fn(self, a="HELLLO!", b=5):
+        print(a, b)
 
 
 app = QApplication(sys.argv)
